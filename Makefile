a886ad8e55bcb0069509b7c1f6fd86fcf1be7465
Erfan Sahebi <erfansahebivayghan@gmail.com>
2023-06-13 05:24:04 +0330

    update

--- Makefile
+++
@@ -1,5 +0,0 @@
-proto:
-	protoc services/**/proto/*.proto --go_out=plugins=grpc:.
-
-server:
-	go run main.go
--- common/error.go
+++
@@ -1,8 +0,0 @@
-package common
-
-import "errors"
-
-var (
-	GenericHTTPServerErr = errors.New("the server has failed to process the request, please do not retry")
-	ErrAccessDenied      = errors.New("access denied")
-)
