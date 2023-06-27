28975023dc665243eb2bc409a11b4ec0331211c4
Erfan Sahebi <erfansahebivayghan@gmail.com>
2023-06-28 00:59:47 +0330

    update

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='lamia_shared',
+    version='1.0.20',
+    description='Lamia Shared Library',
+    author='Erfan Sahebi',
+    packages=find_packages(),
+    install_requires=[
+        'protobuf~=4.23.3',
+        'grpcio~=1.56.0',
+        'grpcio-tools~=1.56.0'
+    ]
+)
