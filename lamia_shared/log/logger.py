2b4df0fbcad3a6548e82621ceac4ef45d50fc736
Erfan Sahebi <erfansahebivayghan@gmail.com>
2023-07-02 15:30:06 +0330

    update

---
+++ lamia_shared/log/logger.py
@@ -0,0 +1,5 @@
+import logging
+
+
+class Log(logging):
+    pass
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='lamia_shared',
-    version='1.0.24',
+    version='1.0.25',
     description='Lamia Shared Library',
     author='Erfan Sahebi',
     packages=find_packages(),
